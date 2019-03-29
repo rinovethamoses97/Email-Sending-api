ab76a8c2a054e1d11418bef5807d958f46223fc5
rino <rinovethamoses97@gmail.com>
2019-03-29 12:14:19 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,35 @@
+var express=require("express");
+var cors=require("cors");
+var nodemailer=require("nodemailer");
+var app=express();
+app.use(cors());
+var transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'emaile729@gmail.com',
+      pass: '#email1234'
+    }
+ });
+app.get('/sendEmail/:to/:content',function(req,res){
+	console.log("Email Sending started");
+	var mailOptions = {
+        from: 'emaile729@gmail.com',
+        to: req.params.to,
+        subject: 'Notification..',
+        text: req.params.content
+    }; 
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+            res.send({msg:"failed"});
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send({msg:"success"});
+        }
+    });
+});
+app.listen(process.env.PORT||3000,function(){
+	console.log("Server running");
+})
